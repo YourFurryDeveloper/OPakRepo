479e9b79e36a7bc94d720c1b88616ce12278a695
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-01-23 15:26:58 +0000

    Update pytxt.py

--- pytxt.py
+++ pytxt.py
@@ -18,7 +18,13 @@ subparsers.add_parser('list', help="List all tasks")
 # Parse the arguments
 args = parser.parse_args()
 
+global isOpen
+isOpen = False
+
 def openTxt(txtfilename):
+	print("PyTXT V1.0 By YourFurryDeveloper on GitHub \n")
+	print("Type 'exittxt' to exit \n----------")
+	isOpen = True
 	lineNum = 0
 	# Open the file in read mode
 	with open(txtfilename, 'r') as file:
@@ -26,7 +32,17 @@ def openTxt(txtfilename):
 		for line in file:
 			# Print the current line
 			lineNum += 1
-			print(str(lineNum) + ". " + line.strip())  # .strip() removes leading/trailing whitespace
+			lineNumHi = "\033[91m" + str(lineNum) + ". " + "\033[0m"
+			print(lineNumHi + line.strip())  # .strip() removes leading/trailing whitespace
+	# Open the file in append mode
+	with open(txtfilename, 'a') as fileapp:
+		while isOpen:
+			txtToApp = input(lineNumHi)
+			if txtToApp == "exittxt":
+				os.system("clear")
+				isOpen = False
+			else:
+				fileapp.write(txtToApp + "\n")
 
 # Handle the subcommands
 if args.command == 'open':
