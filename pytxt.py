edb14d0fc40262aa5d4b724b6d07f579581f457a
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-01-23 15:28:45 +0000

    Update pytxt.py

--- pytxt.py
+++ pytxt.py
@@ -12,8 +12,8 @@ subparsers = parser.add_subparsers(dest='command')
 add_parser = subparsers.add_parser('open', help="Open a file")
 add_parser.add_argument('file', type=str, help="Name of the file to open")
 
-# Define the 'list' subcommand
-subparsers.add_parser('list', help="List all tasks")
+add_parser = subparsers.add_parser('read', help="Open a file in read mode")
+add_parser.add_argument('file', type=str, help="Name of the file to open")
 
 # Parse the arguments
 args = parser.parse_args()
@@ -44,11 +44,26 @@ def openTxt(txtfilename):
 			else:
 				fileapp.write(txtToApp + "\n")
 
+def openTxtRead(txtfilename):
+	print("PyTXT V1.0 By YourFurryDeveloper on GitHub \n")
+	print("Type 'exittxt' to exit \n----------")
+	isOpen = True
+	lineNum = 0
+	# Open the file in read mode
+	with open(txtfilename, 'r') as file:
+		# Loop through each line in the file
+		for line in file:
+			# Print the current line
+			lineNum += 1
+			lineNumHi = "\033[91m" + str(lineNum) + ". " + "\033[0m"
+			print(lineNumHi + line.strip())  # .strip() removes leading/trailing whitespace
+
 # Handle the subcommands
 if args.command == 'open':
 	os.system("clear")
 	openTxt(args.file)
-elif args.command == 'list':
-    list_tasks()
+elif args.command == 'open':
+	os.system("clear")
+	openTxtRead(args.file)
 else:
     parser.print_help()
